ca2c096b246ce29a24487ff624c8adbd8a357267
Caleb Joel <110543111+calebgunner@users.noreply.github.com>
2026-03-30 19:36:59 +0200

    Working on Shooting mechanics
    Transitioned to "Hitscan" shooting from the projectie shooting

--- Assets/_New Prototype/_Scripts/_ProjectileCollision.cs
+++ Assets/_New Prototype/_Scripts/_ProjectileCollision.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class ProjectileCollision : MonoBehaviour
+public class _ProjectileCollision : MonoBehaviour
 {
-
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-           collision.gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
-
 
 }
--- Assets/_New Prototype/_Scripts/_TankControl.cs
+++ Assets/_New Prototype/_Scripts/_TankControl.cs
@@ -17,7 +17,7 @@ public enum TankMovement //USE IT OUTSIDE THE CLASSES TO MAKE THINGS A LOT EASIE
 public class _TankControl : MonoBehaviour
 {
     /// <summary>
-    ///  WORK ON THE SHOOTING MECHANIC NOW
+    ///  SHOOTING LAGS BEHIND WHEN PLAYER IS MOVING AND SHOOTING
     /// </summary>
 
     [HideInInspector] public Rigidbody rb;
@@ -35,8 +35,27 @@ public class _TankControl : MonoBehaviour
     public float dodgeDuration;
     public float canDodgeDelay;
 
+    [Header("shooting mechanics")]
+    public Transform spawnPoint;
+    public GameObject trailPrefab;
+    public bool isOnTarget;
+    public bool isHoldingShoot;
+
+    [Space]
+    public bool isShooting1;
+    public float shootCooldown1; // Cooldown time between attacks
+    bool canShoot1 = true;
+
+    [Space]
+    public LayerMask enemyLayer;
+    public LayerMask otherLayer;
+    RaycastHit hit;
+
+
     [Header("references")]
     _TankCamera tC;
+
+
 
 
     void Start()
@@ -53,9 +72,11 @@ public class _TankControl : MonoBehaviour
         // ANY MOVEMENT WILL TRIGGER THE BOOLEAN
         isMoving = movementInput.magnitude > 0.01f;
 
+
         #region OTHER FUNCTIONS:
 
         StateMachine();
+        AimingDirections();
 
         #endregion
     }
@@ -115,6 +136,189 @@ public class _TankControl : MonoBehaviour
     #endregion
 
 
+    #region SHOOTING MECHANICS:
+
+
+    #region // NOTE FOR SHOOTING MECHANICS //
+
+    // 1. use "HITSCAN" for guns like i.e. machine guns, shot guns etc. since they are very fast and aren't able to be seen
+    // Shoot FROM camera directly
+    // a. these are quick and hit the target quickly without feeling "off" no matter the range
+
+    // (EXAMPLE CODE USED IN THIS SCRIPT FOR PLAYER
+
+
+    // 2. use the "INSTANTIATE PREFABS" for visible, slower attacks like i.e. rockets, slower energy blast
+    // Shoot FROM gun TO where camera is aiming
+    // a. these have an unwated offset at close range.
+
+    //2.1 The code you have made
+    
+    /* void ShootingMechanics()
+    {
+        #region BULLET RELEASED:
+
+        // Instantiate the projectile at the position and rotation of this transform
+        Rigidbody clone;
+        clone = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+
+        #endregion
+
+        #region AIMING / DIRECTION:
+
+        // Calculate the direction from spawnPoint to the center of the screen
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            // This block runs if the ray hits something
+            targetPoint = hit.point;
+
+        }
+        else
+        {
+            // This block runs if the ray does NOT hit anything
+            targetPoint = ray.GetPoint(2000);
+
+            // ** MAKE AN INVISIBLE BORDER SO THAT THE RAY TECHNICALLY IS ALWAYS HITTING AN OBJECT THAT THE AIM ALWAYS WORKS **
+        }
+
+        // Calculate direction
+        Vector3 direction = (targetPoint - spawnPoint.position).normalized;
+
+        #endregion
+
+        #region BULLET VELOCITY / COLLISION:
+
+        // Apply velocity to the projectile
+        clone.linearVelocity = direction * bulletSpeed;
+
+        //Ignore Collision Between Player and Projectile [TO PREVENT THE UNCONTROLLED RECOIL]
+        Physics.IgnoreCollision(clone.GetComponent<Collider>(), rb.GetComponent<Collider>());
+
+        #endregion
+
+    } */
+
+
+    #endregion
+
+
+    //THIS HELPS TO SEE WHETHER THE PLAYER IS ON TARGET
+    void AimingDirections() 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+        float maxDistance = Mathf.Infinity;
+
+        if (Physics.Raycast(ray, out hit, maxDistance, enemyLayer))
+        {
+            isOnTarget = true; //the ray hit something
+
+        }
+        else
+        {
+            isOnTarget = false; //the ray did not hit anything
+
+            // ** MAKE AN INVISIBLE BORDER SO THAT THE RAY TECHNICALLY IS ALWAYS HITTING AN OBJECT THAT THE AIM ALWAYS WORKS **
+        }
+    }
+
+
+    void ShootingMechanics() // Hitscan
+    {
+        #region AIMING / DIRECTION:
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)); //creates ray from the center of our screen
+
+        Vector3 targetPoint; //where the bullet should go
+
+        #endregion
+
+        #region HITSCAN LOGIC:
+
+        if (Physics.Raycast(ray, out hit, 2000f)) //shoots invisible line forward
+        {
+            targetPoint = hit.point;
+
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                // Apply damage here
+                // hit.transform.GetComponent<Enemy>().TakeDamage();
+            }
+
+            Debug.DrawLine(ray.origin, hit.point, Color.red, 0.2f);
+        }
+        else
+        {
+            targetPoint = ray.origin + ray.direction * 2000f;
+
+            Debug.DrawLine(ray.origin, targetPoint, Color.red, 0.2f);
+        }
+
+        #endregion
+
+        #region SHOOTING TRAIL CODE:
+
+        GameObject trail = Instantiate(trailPrefab, spawnPoint.position, Quaternion.identity); //spawns the trail
+        StartCoroutine(MoveTrail(trail, targetPoint));
+
+        #endregion
+    }
+
+
+    IEnumerator MoveTrail(GameObject trail, Vector3 targetPos) //moves the trail
+    {
+        Vector3 startPos = trail.transform.position;
+
+        float time = 0f;
+
+        while (time < 0.05f)
+        {
+            trail.transform.position = Vector3.Lerp(startPos, targetPos, time / 0.05f); //smoothly goes between pos 1 to pos 2
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        trail.transform.position = targetPos;
+
+        Destroy(trail, 0.1f);
+    }
+
+
+    IEnumerator ShootingControl()
+    {
+        while (isHoldingShoot && canShoot1)
+        {
+            isShooting1 = true;
+            canShoot1 = false;
+
+            ShootingMechanics(); // Apply the shooting mechanics
+
+            // Return to shoot-idle or appropriate state
+            //theMovementType = MovementType.Normal_Shot;
+            //movementText.text = "Movement : normal-shot";
+
+            //Turn off effects
+            //NormalShot_Particle.SetActive(true);
+
+            // Wait for shoot animation or duration
+            yield return new WaitForSeconds(0.17f);
+
+            isShooting1 = false;
+
+            //NormalShot_Particle.SetActive(false);
+
+            // Wait for cooldown
+            yield return new WaitForSeconds(shootCooldown1); //ZERO SINCE THE "SHOOTING ANIMATION" IS A LONG ENOUGH WAIT
+            canShoot1 = true;
+        }
+    }
+
+    #endregion
+
+
     #region STATE MACHINE:
 
     void StateMachine() //CONTROLLING THE STATE OF THE TANK
@@ -156,6 +360,24 @@ public class _TankControl : MonoBehaviour
             StartCoroutine(Dodge());
         }
     }
+
+
+    // SHOOT
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.performed) //When the button is pressed
+        {
+            isHoldingShoot = true;
+            StartCoroutine(ShootingControl());
+        }
+
+        else if (context.canceled)
+        {
+            isHoldingShoot = false;
+        }
+    }
+
+
 
     #endregion
 }
